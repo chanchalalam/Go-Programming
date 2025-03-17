4217878471387772e90781fc0252a038bcb3933a
Chanchal Alam <106960500+chanchalalam@users.noreply.github.com>
2025-03-17 16:31:21 +0530

    Add files via upload

---
+++ Keyword.go
@@ -0,0 +1,46 @@
+package main
+import "fmt"
+
+
+func main(){
+	var Pname = "Chanchal Alam"
+	var Lname = "GO, Python, Java, R"
+	var Rname = "Data Scientist"
+
+	fmt.Println("Name: ", Pname)
+	fmt.Println("Language: ", Lname)
+	fmt.Println("Role: ", Rname)
+
+}
+
+
+
+/*There are total 25 keywords present in the Go language as follows:
+
+break
+case
+char
+const
+continue
+default
+defer
+else
+fallthrough
+for
+func
+go
+goto
+if
+import
+interface
+map
+package
+range
+return
+select
+struct
+switch
+type
+var
+
+*/
